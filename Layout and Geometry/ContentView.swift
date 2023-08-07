9ea7c26623d7888914f3de09db18e0c9732e4613
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-07 16:44:23 -0500

    experiment with ScrollView effects

--- Layout and Geometry/ContentView.swift
+++ Layout and Geometry/ContentView.swift
@@ -8,63 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        // take 90% of all available width regardless of content
-        GeometryReader { geo in
-            Text("Hello, world!")
-                .frame(width: geo.size.width * 0.9)
-                .background(.red)
-        }
-        
-        // Side effect: the view that gets returned has a flexible preferred size;
-        // meaning it will expand to take up more space as needed
-        VStack {
-            GeometryReader { geo in
-                Text("Hello, world!")
-                    .frame(width: geo.size.width * 0.9, height: 40)
-                    .background(.red)
-            }
-            // this green bg shows us just how big it is
-            // note: preferred size, NOT absolute
-            .background(.green)
-            
-            // gets pushed to the bottom
-            Text("More text")
-                .background(.blue)
-        }
-        
-        OuterView()
-            .background(.red)
-            .coordinateSpace(name: "Custom")
-    }
-}
-
-struct OuterView: View {
-    var body: some View {
-        VStack {
-            Text("Top")
-            InnerView()
-                .background(.green)
-            Text("Bottom")
-        }
-    }
-}
-
-struct InnerView: View {
-    var body: some View {
-        HStack {
-            Text("Left")
-            GeometryReader { geo in
-                Text("Center")
-                    .background(.blue)
-                    .onTapGesture {
-                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
-                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
-                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+        // 3D rotating helix
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
                     }
+                    .frame(height: 40)
+                }
             }
-            .background(.orange)
-            Text("Right")
+        }
+        
+        // Cover-flow style scrolling rectangles
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    GeometryReader { geo in
+                        Text("Number \(num)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                            .frame(width: 200, height: 200)
+                    }
+                    .frame(width: 200, height: 200)
+                }
+            }
         }
     }
 }
