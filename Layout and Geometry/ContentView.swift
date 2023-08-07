23648cc50b53430d253134652d5e8163a0e2d09f
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-07 16:19:04 -0500

    absolute positioning, changing where a view is rendered w/o affecting its geometry

--- Layout and Geometry/ContentView.swift
+++ Layout and Geometry/ContentView.swift
@@ -9,37 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@thechiassonator")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                Image("Example")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack {
-                Text("Full name:")
-                Text("KEENAN CHIASSON")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                    .font(.largeTitle)
-            }
-        }
+        // absolute positioning
+        Text("Hello, world!")
+            .position(x: 100, y: 100)
+        
+        // red background tightly fits
+        Text("Hello, world!")
+            .background(.red)
+            .position(x: 100, y: 100)
+        
+        // position provides new view that takes all available space
+        Text("Hello, world!")
+            .position(x: 100, y: 100)
+            .background(.red)
+        
+        // remember the 3-step layout process of SwiftUI
+        // 1. Parent view proposes a size for its child
+        // 2. Child chooses its size, parent must respect choice
+        // 3. Parent positions child in its coordinate space
+        
+        // the .offset() modifier changes the location
+        // of where a view should be rendered,
+        // but does NOT actually change its underlying geometry
+        // .background() uses the orginal position of text, NOT the offset
+        Text("Hello, world!")
+            .offset(x: 100, y: 100)
+            .background(.red)
+        
+        // this might be more what you'd expect,
+        // illustrating MODIFIER ORDER MATTERS
+        Text("Hello, world!")
+            .background(.red)
+            .offset(x: 100, y: 100)
     }
-}
-
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
 }
 
 struct ContentView_Previews: PreviewProvider {
