922e0fa72364f16c3ef5daab1c070b499fcb5d8a
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-07 12:34:37 -0500

    Initial Commit

---
+++ Layout and Geometry/Layout_and_GeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_and_GeometryApp.swift
+//  Layout and Geometry
+//
+//  Created by Keenan Chiasson on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Layout_and_GeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
